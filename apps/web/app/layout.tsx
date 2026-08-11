19d4d161fded05e8227108886ef0a4ee868f167d
Kunal Vaidya <kvaidya@squarespace.com>
2026-08-11 10:54:34 +0100

    Revert "feat: update GA4 measurement ID to G-7JGXF8343D"
    This reverts commit 6eaff175a17b2bc77e6eaabaaa9986155de78737.

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={`${lato.variable} ${instrumentSerif.variable}`}>
       <head>
         <AnalyticsScripts
-          ga4Id="G-7JGXF8343D"
+          ga4Id="G-3YD9S3SJ63"
           {...(process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN ? { plausibleDomain: process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN } : {})}
         />
       </head>
